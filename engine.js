54cd12f4a4966f16bdac4d3f5a798f3a544834da
Ryan <rtoole13@gmail.com>
2019-03-04 23:27:26 -0500

    Beginning work on some Perlin noise field stuff. Pulling heavily from the tutorial of Johan Karlsson, who made use of Seph Gentle's js conversion of Stefan Gustavson's noise library.

--- engine.js
+++ engine.js
@@ -1,5 +1,131 @@
 "use strict";
 
+class Grid {
+    constructor(rows, columns, width, height){
+        this.rows = rows;
+        this.columns = columns;
+        this.width = width;
+        this.height = height;
+        this.gridSpacing = {x:null, y:null};
+        this.elem = [];
+        this.initializeNodes();
+    }
+
+    initializeNodes(){
+
+        this.gridSpacing.x = this.width / this.columns;
+        this.gridSpacing.y = this.height / this.rows;
+
+        for (var i = 0; i < this.columns; i++){
+            var xLoc = this.gridSpacing.x/2 + this.gridSpacing.x * i;
+            var rowArray = [];
+            for (var j = 0; j < this.rows; j++){
+                var yLoc = this.gridSpacing.y/2 + this.gridSpacing.y * j;
+                var node = new GridNode(xLoc, yLoc, this.gridSpacing.x, this.gridSpacing.y, i, j);
+                rowArray.push(node);
+            }
+            this.elem.push(rowArray);
+        }
+    }
+
+    getNodeFromLocation(x, y){
+        var i,j;
+        i = Math.floor(x / this.gridSpacing.x);
+        j = Math.floor(y / this.gridSpacing.y);
+
+        if (i >= 0 && i < this.columns && j >= 0 && j < this.rows){
+            return this.elem[i][j];
+        }
+        return null;
+    }
+
+    getAllNodesInRadius(x, y, radius){
+        var nodes, radiusSq, rootNode, xExtent, yExtent;
+        nodes = [];
+        radiusSq = radius * radius;
+        rootNode = this.getNodeFromLocation(x, y);
+        nodes.push(rootNode);
+
+        xExtent = Math.ceil(radius / this.gridSpacing.x);
+        yExtent = Math.ceil(radius / this.gridSpacing.y);
+        for (var i = -xExtent; i <= xExtent; i++){
+            for (var j = -yExtent; j <= yExtent; j++){
+                var checkX = rootNode.indX + i;
+                var checkY = rootNode.indY + j;
+
+                if (checkX >= 0 && checkX < this.columns && checkY >= 0 && checkY < this.rows){
+                    var elem = this.elem[checkX][checkY];
+                    if (getDistanceSq(x, y, elem.x, elem.y) <= radiusSq){
+                        nodes.push(elem);
+                    }
+                }
+            }
+        }
+        return nodes;
+    }
+
+    getClosestValidNodeFromLocation(x, y){
+        var i,j;
+        i = Math.floor(x / this.gridSpacing.x);
+        j = Math.floor(y / this.gridSpacing.y);
+
+        if (i < 0){
+            i = 0;
+        }
+        else if (i >= this.columns){
+            i = this.columns - 1;
+        }
+        if (j < 0){
+            j = 0;
+        }
+        else if (j >= this.rows){
+            j = this.rows - 1;
+        }
+        return this.elem[i][j];
+    }
+    update(){
+        for (var i = 0; i < this.columns; i++){
+            for (var j = 0; j < this.rows; j++){
+                this.elem[i][j].update();
+            }
+        }
+    }
+    draw(){
+        for (var i = 0; i < this.columns; i++){
+            for (var j = 0; j < this.rows; j++){
+                this.elem[i][j].draw();
+            }
+        }
+    }
+}
+
+class GridNode{
+    constructor(x, y, width, height, indX, indY){
+        this.x = x;
+        this.y = y;
+        this.indX = indX;
+        this.indY = indY;
+        this.width = width;
+        this.height = height;
+        this.angle = noise.simplex3(this.indX / 50, this.indY / 50, timeFactor) * Math.PI * 2;
+        this.length = noise.simplex3(this.indX / 100, this.indY / 100 + 40000, timeFactor);
+    }
+    update(){
+        this.angle = noise.simplex3(this.indX / 50, this.indY / 50, timeFactor) * Math.PI * 2;
+        this.length = noise.simplex3(this.indX / 100, this.indY / 100 + 40000, timeFactor);
+    }
+    draw(){
+        canvasContext.save();
+        canvasContext.strokeStyle = "black";
+        canvasContext.translate(this.x, this.y);
+        canvasContext.rotate(this.angle);
+        canvasContext.beginPath();
+        canvasContext.moveTo(0, 0);
+        canvasContext.lineTo(0, this.width * this.length);
+        canvasContext.stroke();
+        canvasContext.restore();
+    }
+}
 function circleOnCircle(circleA, circleB){
     var distsq = getDistanceSq(circleA.x, circleA.y, circleB.x, circleB.y);
     if (distsq <= Math.pow(circleA.radius + circleB.radius, 2)){
